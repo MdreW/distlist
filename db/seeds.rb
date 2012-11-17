799b4fce19400b822af6cccbcfbec3c7807ab036
MdreW <andrea.ranaldi@gmail.com>
2012-11-17 15:48:40 +0100

    Added some example (campaigns/emails/addresses)

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,14 @@
 
 User.create(:email => 'admin@distlist.com', :password => '12345678')
 User.first.to_admin!
-User.first.campaigns.create(:title => "example", :sender_name => "name", :sender_email => "example@mail.com", :time_gap => 0)
-Campaign.first.addresses.create(:email => "example@mail.com", :name => "name", :surname => "surname", :options_attributes => [{:key => "key1", :value => "value1"}])
+User.create(:email => 'user@distlist.com', :password => '12345678')
+User.first.campaigns.create(:title => "Example campaign", :sender_name => "name", :sender_email => "example@distlist.com", :header => "<p>A common header for each email</p>", :footer => "</p>A common footer for each email</p>", :time_gap => 0, :unsubscribe => true)
+Campaign.first.addresses.create(:email => "user1@distlist.com", :name => "user1", :surname => "surname1", :options_attributes => [{:key => "key1", :value => "value1"}, {:key => "key1", :value => "value1"}])
+Campaign.first.addresses.create(:email => "user1@distlist.com", :name => "user2", :surname => "surname2", :options_attributes => [{:key => "key1", :value => "value1"}, {:key => "key1", :value => "value1"}])
+Campaign.first.addresses.create(:email => "user1@distlist.com", :name => "user3", :surname => "surname3", :options_attributes => [{:key => "key1", :value => "value1"}, {:key => "key1", :value => "value1"}])
+Campaign.first.addresses.create(:email => "user1@distlist.com", :name => "user4", :surname => "surname4", :options_attributes => [{:key => "key1", :value => "value1"}, {:key => "key1", :value => "value1"}])
+Campaign.first.addresses.create(:email => "user1@distlist.com", :name => "user5", :surname => "surname5", :options_attributes => [{:key => "key1", :value => "value1"}, {:key => "key1", :value => "value1"}])
+Campaign.first.addresses.create(:email => "user1@distlist.com", :name => "user6", :surname => "surname6", :options_attributes => [{:key => "key1", :value => "value1"}, {:key => "key1", :value => "value1"}])
+Campaign.first.emails.create(:subject => "News for you Mr. {name} {surname}!", :body => "<p><b>The text replacement work correctly!</b></p><ul><li>Key1 is {key1}</li><li>Key2 is {key2}</li></ul>")
+Campaign.first.emails.create(:subject => "Other example... {name} {key}", :body => "<p>Bye bye {surname}</p>")
+Campaign.first.emails.create(:subject => "Last example bla bla", :body => "<h2>Stress Test!</h2><p><!{{{name}/{surname@}\{key}}</p>")
